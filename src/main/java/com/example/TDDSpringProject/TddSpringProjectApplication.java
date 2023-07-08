9241b197c94e7dfb25fbfd620c86bec8f7bc9980
Cierra <ceceminorhampton@gmail.com>
2023-07-07 22:07:31 -0400

    cleaned up code

--- src/main/java/com/example/TDDSpringProject/TddSpringProjectApplication.java
+++ src/main/java/com/example/TDDSpringProject/TddSpringProjectApplication.java
@@ -12,18 +12,18 @@ public class TddSpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TddSpringProjectApplication.class, args);
-	}
-	@Bean
-	public CommandLineRunner run(OrderServices orderServices){
-		return (args -> {
-			insertTest(orderServices);
-			//System.out.println(orderServices.readOrderList());
-		});
-	}
-	private void insertTest(OrderServices orderServices){
-		//orderServices.createOrder(new OrderEntity("CustomerName",LocalDate.now(),"123 Main Plano,Tx 75025",11.00));
-		//orderServices.deleteOrderById(Long.valueOf(1552));
-		//orderServices.updateOrder(new OrderEntity("Updated CustomerName",LocalDate.now(),"123 New Main Plano,Tx 75025",12.00),Long.valueOf(1602));
-		}
+	 }
+	// @Bean
+	// public CommandLineRunner run(OrderServices orderServices){
+	// 	return (args -> {
+	// 		insertTest(orderServices);
+	// 		//System.out.println(orderServices.getAllOrders());
+	// 	});
+	// }
+	// private void insertTest(OrderServices orderServices){
+	// 	//orderServices.createOrder(new OrderEntity("CustomerName",LocalDate.now(),"123 Main Plano,Tx 75025",11.00));
+	// 	//orderServices.deleteOrderById(Long.valueOf(1552));
+	// 	//orderServices.update(new OrderEntity("Updated CustomerName",LocalDate.now(),"123 New Main Plano,Tx 75025",12.00),Long.valueOf(1602));
+	// 	}
 
 }
